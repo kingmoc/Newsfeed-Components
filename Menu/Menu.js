1e3f8094811b652c54dd285756cbde085e024845
kingmoc <joe.c.a.garcia@gmail.com>
2019-05-15 10:44:12 -0500

    Added Menu Animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,25 @@
 
+// Start Here: Create a reference to the ".menu" class
+const menu = document.querySelector('.menu');
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open')
+  if(menu.classList.contains('menu--open')) {
+      TweenMax.to(menu, .8, {
+        x: 500,    
+        ease: Power4.easeOut,
+      });
+      console.log('opening');
+  }
+  else {
+      TweenMax.to(menu, 1.5, {
+        x: -500,    
+        ease: Power4.easeOut,
+      });
+      console.log('closing');
+  }
 }
-
-// Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
-console.log(menu);
 
 
 // create a reference to the ".menu-button" class
@@ -16,4 +29,6 @@ console.log(menuButton);
 
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', e => toggleMenu())
+menuButton.addEventListener('click', e => {
+  toggleMenu()
+})
